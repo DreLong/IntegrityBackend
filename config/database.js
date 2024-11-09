afb48b6dc3ebb6f383fff2a7785f46758430326f
Oluwadamilare Alonge <59781498+DreLong@users.noreply.github.com>
2024-11-09 02:15:06 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,10 +2,16 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // Include the custom port here
+    dialect: 'postgres',
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
